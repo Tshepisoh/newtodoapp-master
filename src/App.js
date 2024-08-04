ff91fb75d1d75830c4615eafe47880d6bbaeffa4
Tshepisoh <thlahla4@gmail.com>
2024-08-04 20:53:01 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './components/Main';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, createTheme, CssBaseline, IconButton, Box } from '@mui/material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
+import './App.css';
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [mode, setMode] = useState('light');
+
+  useEffect(() => {
+    const storedMode = localStorage.getItem('themeMode');
+    if (storedMode) {
+      setMode(storedMode);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newMode = mode === 'light' ? 'dark' : 'light';
+    setMode(newMode);
+    localStorage.setItem('themeMode', newMode);
+  };
+
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <QueryClientProvider client={queryClient}>
+          <Box sx={{ position: 'absolute', top: 16, right: 16 }}>
+            <IconButton onClick={toggleTheme} color="inherit">
+              {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+            </IconButton>
+          </Box>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/Main" element={<Main />} />
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/CreateTodos.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { Box, Button, FormControl, OutlinedInput } from '@mui/material';
+
+const CreateTodo = ({ handleSubmit }) => {
+  return (
+    <Formik
+      initialValues={{ text: '' }}
+      onSubmit={(values, { resetForm }) => {
+        handleSubmit(values);
+        resetForm();
+      }}
+    >
+      {({ handleChange, handleBlur, values }) => (
+        <Form>
+          <Box sx={{ display: 'flex', alignItems: 'center', mb: 2,  }}>
+            <FormControl sx={{ flexGrow: 1, }}>
+              <OutlinedInput
+                placeholder="Add Task..."
+                required
+                type="text"
+                name="text"
+                value={values.text}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                sx={{ backgroundColor: 'white', borderRadius: 1 }}
+              />
+            </FormControl>
+            <Button type="submit" variant="contained" color="primary" sx={{ ml: 2, height: '100%' }}>
+              Add
+            </Button>
+          </Box>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default CreateTodo;
